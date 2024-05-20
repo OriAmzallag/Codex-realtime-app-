3f9842baa28aabb5a60386003fe3c409931c161a
Ori <ortube54@gmail.com>
2024-05-21 01:02:36 +0300

    Added my project

--- server/routers/routes.js
+++ server/routers/routes.js
@@ -7,7 +7,10 @@ router.get("/:caseName", async (req, res) => {
   try {
     const { caseName } = req.params;
     const code = await getCodeBlockByCaseName(caseName);
-    res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+    res.setHeader(
+      "Access-Control-Allow-Origin",
+      "https://codex-realtime-app.netlify.app"
+    );
     res.send(code);
   } catch (error) {
     console.error(`Error fetching code for ${req.params.caseName}:`, error);
